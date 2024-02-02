813f8b8ed730b0af2c1de73440bc6d8ad5ac0550
Robertson Brinker <robertsonbrinker@gmail.com>
2024-02-02 13:52:32 -0500

    working chinese tokenizer

--- kathairo/Tokenization/ChineseBibleWordTokenizer.py
+++ kathairo/Tokenization/ChineseBibleWordTokenizer.py
@@ -2,32 +2,31 @@ from Tokenization import MaximalMatchingTokenizer
 import os.path
 import os
 from typing import Optional
-from pathlib import Path
-#using System.Reflection;
 
-#namespace ClearBible.Engine.Tokenization
-#{
-    # <summary>
-    # A tokenizer for bible translations (not general Chinese translations, see below).
-    # 
-    # Chinese words and combination corrections provided by Andi Wu (andi.wu@globalbibleinitiative.org)
-    # </summary>
-class ChineseBibleWordTokenizer(MaximalMatchingTokenizer):
-#{
-    WORDS_FILE_NAME = "words.txt"
-    COMBINATION_CORRECTIONS_FILE_NAME = "combination_corrections.txt"
+# <summary>
+# A tokenizer for bible translations (not general Chinese translations, see below).
+# 
+# Chinese words and combination corrections provided by Andi Wu (andi.wu@globalbibleinitiative.org)
+# </summary>
 
-    def __init__(self):#, chineseTokenizerDataDirectoryPath: Optional[str] = None, max_gram: Optional[int] = None): 
-        #if max_gram is None:
-        max_gram = 10
+class ChineseBibleWordTokenizer(MaximalMatchingTokenizer.MaximalMatchingTokenizer):
+   
+
+    def __init__(self,chineseTokenizerDataDirectoryPath: Optional[str] = None,max_gram: Optional[int] = None):
+        if max_gram is None:
+            max_gram = 10
+        self.WORDS_FILE_NAME = "words.txt"
+        self.COMBINATION_CORRECTIONS_FILE_NAME = "combination_corrections.txt"
         super().__init__(max_gram)
    
         # set Words and overlaps.
         dataDirectoryFilePath = chineseTokenizerDataDirectoryPath 
         if dataDirectoryFilePath is None:
             #Path(__file__).parent / "Tokenization" / "Data" / "ChineseBibleWordTokenizer"
-            wordsFilePath = os.path.join(os.getcwd,"Tokenization","Data","ChineseBibleWordTokenizer", WORDS_FILE_NAME)
-            combinationsFilePath = os.path.join(os.getcwd,"Tokenization","Data","ChineseBibleWordTokenizer", COMBINATION_CORRECTIONS_FILE_NAME)
+            currentWorkingDirectory = os.getcwd()
+            appName = "kathairo"
+            wordsFilePath = os.path.join(currentWorkingDirectory,appName,"Tokenization","Data","ChineseBibleWordTokenizer", self.WORDS_FILE_NAME)
+            combinationsFilePath = os.path.join(currentWorkingDirectory,appName,"Tokenization","Data","ChineseBibleWordTokenizer", self.COMBINATION_CORRECTIONS_FILE_NAME)
                        
         with open(wordsFilePath, "r", encoding='utf-8') as words_file:
             #lines = words_file.readlines()
@@ -48,6 +47,3 @@ class ChineseBibleWordTokenizer(MaximalMatchingTokenizer):
                 parts = line.strip().split("\t")
                 if parts[0] not in self.CombinationCorrections:
                     self.CombinationCorrections[parts[0]] = parts[1]
-    #}
-#}
-#}
