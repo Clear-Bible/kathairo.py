15a1bd3a4c5d12dbcea55d2fa6f876ab3c85a631
Jake Wegner <jake.wegner@biblica.com>
2024-03-01 11:35:54 -0600

    Add reconstituted files

---
+++ proposals/reconstitute.py
@@ -0,0 +1,36 @@
+import sys
+import csv
+from pathlib import Path
+
+
+def main():
+    directory = Path(sys.argv[1])
+    input_files = directory.glob("*.tsv")
+    for input_file in input_files:
+        rows = [r for r in csv.DictReader(input_file.open(), delimiter="\t")]
+        verses = []
+        parts = []
+        last_verse = None
+        for row in rows:
+            this_verse = row["id"][0:8]
+            if last_verse is None:
+                last_verse = this_verse
+                parts.append(f"{this_verse} ")
+
+            if last_verse != this_verse:
+                verses.append("".join(parts).strip())
+                last_verse = this_verse
+                parts = [f"{this_verse} "]
+
+            parts.append(row["text"])
+            if row["space_after"] == "n":
+                continue
+            parts.append(" ")
+        verses.append("".join(parts).strip())
+
+        output_file = input_file.parent / f"{input_file.stem}.txt"
+        output_file.write_text("\n".join(verses))
+
+
+if __name__ == "__main__":
+    main()
