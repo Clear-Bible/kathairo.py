95961c75c9d7eb2be1d8e0adc324a0c1e0dd165e
Robertson Brinker <robertsonbrinker@gmail.com>
2024-03-11 15:27:48 -0400

    working ID validation

--- test/__init__.py
+++ test/__init__.py
@@ -6,6 +6,9 @@ __macula_greek_tsv_rows__ = []
 tsv_path = "../TSVs/target_BSB_new.tsv"
 __tsv_files__ = [tsv_path]
 
+vrs_path = "../resources/bsb_usx/release/versification.vrs"
+__vrs_files__ = [vrs_path]
+
 with open(tsv_path, encoding='utf-8') as file:
     reader = csv.DictReader(file, delimiter="\t")
     for row in reader:
--- test/test_tsv.py
+++ test/test_tsv.py
@@ -1,10 +1,10 @@
 #Based on https://github.com/Clear-Bible/macula-greek/blob/main/test/test_tsv.py
 import os
 import codecs
-import re
 import pytest
-from test import __macula_greek_tsv_rows__, __tsv_files__
+from test import __macula_greek_tsv_rows__, __tsv_files__, __vrs_files__
 import pandas as pd
+from machine.scripture import Versification
 
 # Verify that the file exists.
 @pytest.mark.parametrize("tsv_file", __tsv_files__)
@@ -18,28 +18,74 @@ def test_file_is_valid_utf8(tsv_file):
     lines = codecs.open(tsv_file, encoding="utf-8", errors="strict").readlines()
     assert lines != ""
 
-#do the ids only contain numbers
+#Do the IDs only contain numbers?
 @pytest.mark.parametrize("tsv_file", __tsv_files__)
 def test_id_numeric(tsv_file):
-    dataFrame = pd.read_csv(tsv_file, sep='\t',dtype=str)
-    for id in dataFrame['id']:
+    data_frame = pd.read_csv(tsv_file, sep='\t',dtype=str)
+    for id in data_frame['id']:
         assert id.isnumeric()
 
-
-#are the ids valid length
+#Are the IDs valid length?
 @pytest.mark.parametrize("tsv_file", __tsv_files__)
 def test_id_length(tsv_file):
-    dataFrame = pd.read_csv(tsv_file, sep='\t',dtype=str)
-    for id in dataFrame['id']:
+    data_frame = pd.read_csv(tsv_file, sep='\t',dtype=str)
+    for id in data_frame['id']:
         assert len(str(id)) == 11
 
-#are the ids valid values (requires versification file)
+#Is the Book ID a valid value? (requires versification file)
+#Is the Book in the Versification file? 
 @pytest.mark.parametrize("tsv_file", __tsv_files__)
-def test_id_length(tsv_file):
-    dataFrame = pd.read_csv(tsv_file, sep='\t',dtype=str)
-    for id in dataFrame['id']:
-        assert len(str(id)) == 11
+@pytest.mark.parametrize("vrs_file", __vrs_files__)
+def test_id_book_value(tsv_file, vrs_file):
+    present_book_id_list = []
+    current_book_number = 0
+    
+    targetVersification = Versification.load(vrs_file, fallback_name="web")
+    for book in targetVersification.book_list:
+        current_book_number+=1
+        if(not(len(book) == 1 and book[0] == 1)):
+            present_book_id_list.append(current_book_number)
+    
+    data_frame = pd.read_csv(tsv_file, sep='\t',dtype=str)
+    for id in data_frame['id']:
+        book_id = int(str(id)[:2])
+        assert (book_id > 0 and book_id in present_book_id_list)
 
+#Is the Chapter ID a valid value? (requires versification file)
+#Are the 3rd through 5th digits between 1 and the max chapter value in the versification
+@pytest.mark.parametrize("tsv_file", __tsv_files__)
+@pytest.mark.parametrize("vrs_file", __vrs_files__)
+def test_id_chapter_value(tsv_file, vrs_file):    
+    max_chapter_number = 0
+    
+    targetVersification = Versification.load(vrs_file, fallback_name="web")
+    for book in targetVersification.book_list:
+        book_size = len(book)
+        if(book_size > max_chapter_number):
+                max_chapter_number = book_size
+                
+    data_frame = pd.read_csv(tsv_file, sep='\t',dtype=str)
+    for id in data_frame['id']:
+        chapter_id = int(str(id)[2:5])
+        assert (chapter_id > 0 and chapter_id <= max_chapter_number)
+
+#Is the verse ID a valid value? (requires versification file)
+#Are the 6th-8th digits between 1 and the max verse value in the versification
+@pytest.mark.parametrize("tsv_file", __tsv_files__)
+@pytest.mark.parametrize("vrs_file", __vrs_files__)
+def test_id_verse_value(tsv_file, vrs_file):
+    max_verse_number = 0
+    
+    targetVersification = Versification.load(vrs_file, fallback_name="web")
+    for book in targetVersification.book_list:
+        for chapter_size in book:
+            if(chapter_size > max_verse_number):
+                max_verse_number = chapter_size
+                
+    data_frame = pd.read_csv(tsv_file, sep='\t',dtype=str)
+    for id in data_frame['id']:
+        verse_id = int(str(id)[-3:])
+        assert (verse_id > 0 and verse_id <= max_verse_number)
 
 
 #does each chapter have the number of verses listed in the versification
