83badd1ed93ca652589e2db1b2dfd9d80f07afee
Robertson Brinker <robertsonbrinker@gmail.com>
2024-03-22 11:00:10 -0400

    checking multiple usfm/usx files at once

--- README.md
+++ README.md
@@ -9,4 +9,7 @@ Either:
 - Uncomment/Add appropriate project details to `build_tsv.py`
 
 # Run Unit Tests
-On the commandline, in `\kathairo.py`, run `py.test.exe`.  Alternatively, run `py.test.exe -s` if you want to see the output of the optional tests.
+On the command-line, in `\kathairo.py`, run `py.test.exe`.  Alternatively, run `py.test.exe -s` if you want to see the output of the optional tests.
+
+# WIP
+- USFM/USX validation
--- kathairo/usfmValidator.py
+++ kathairo/usfmValidator.py
@@ -1,28 +1,51 @@
 from usfm_grammar import USFMParser
 from lxml import etree
 
-#use the parser to check our USFM and USX for Errors
-#Then be able to check them all at once in a row
-#then tie that in to the other stuff such that we have
-#Pre-Test
-    #validate the raw USFM and USX files
-    #report
-#TSV Generation
-    #For each file that doesn't have issues, make a TSV
-#Run post generation tests on the quality of the TSVs
+def print_errors_for_usx(file_path:str):
+    with open(file_path, 'r', encoding='utf-8') as usx_file:
+        usx_str = usx_file.read()
+        usx_obj = etree.fromstring(bytes(usx_str, encoding='utf8'))
+        my_parser = USFMParser(from_usx=usx_obj)
+        return my_parser
 
-test_xml_file = "./resources/bsb_usx/release/USX_1/ZEC.usx"
-with open(test_xml_file, 'r', encoding='utf-8') as usx_file:
-    usx_str = usx_file.read()
-    usx_obj = etree.fromstring(bytes(usx_str, encoding='utf8'))
+def print_errors_for_usfm(file_path:str):    
+    input_usfm_str = open(file_path,"r", encoding='utf8').read()
+    my_parser = USFMParser(input_usfm_str)
+    return my_parser
 
-    my_parser = USFMParser(from_usx=usx_obj)
+import os
+
+#directory_path = "./resources/bsb_usx/release/USX_1/"
+directory_path = "./resources/ru_rsb/"
+file_list = os.listdir(directory_path)
+
+for file in file_list:
+    #my_parser = print_errors_for_usx(directory_path+file)
+    my_parser = print_errors_for_usfm(directory_path+file)
+    
     errors = my_parser.errors
-    print(errors)
-    # print(my_parser.to_usj())
-    # print(my_parser.to_list())
+    if(errors is not None):
+        for error in errors:
+            #print(error)
 
-#input_usfm_str = open("./resources/bsb_usfm/50EPHBSB.SFM","r", encoding='utf8').read()
-#my_parser = USFMParser(input_usfm_str)
+            with open('output_russian.txt', 'a') as f:
+                f.write(str(error))   
+                f.write("\n")    
+
+
+#--------------------------------------------
+
+#test_xml_file = "./resources/bsb_usx/release/USX_1/1CH.usx"
+#test_usfm_file = "./resources/bsb_usfm/50EPHBSB.SFM"
+
+#my_parser = print_errors_for_usx(test_xml_file)
+#my_parser = print_errors_for_usfm(test_usfm_file)
+
 #errors = my_parser.errors
-#print(errors)
+#for error in errors:
+#    print(error)
+
+#--------------------------------------------
+
+# print(my_parser.to_usj())
+# print(my_parser.to_list())
