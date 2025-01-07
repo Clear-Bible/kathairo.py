dac02884a34f97cda474c81f282735d8f1550887
Robertson Brinker <robertsonbrinker@gmail.com>
2025-01-07 16:23:40 -0500

    pushing to pypi

--- kathairo/experiments/CUVMPS/select.py
+++
@@ -1,7 +0,0 @@
-import polars as pl
-
-df = pl.read_csv("nt_CUVMPS.tsv", separator='\t', infer_schema_length=0)
-
-results = df.select([pl.col("id"), pl.col("text")])
-
-results.write_csv("nt_CUVMPS_minimum.tsv", separator='\t')
