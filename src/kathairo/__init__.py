8d51beb70b2efdc855d95c8c7d94a58e641c5008
Robertson Brinker <60048070+robertsonbrinker@users.noreply.github.com>
2026-01-29 16:26:22 -0500

    basic builder

--- src/kathairo/__init__.py
+++ src/kathairo/__init__.py
@@ -1,3 +1,19 @@
 from .api import create_tsv
+from .builder import (
+    TSVBuilder,
+    from_usfm_corpus,
+    from_usx_corpus,
+    from_tsv,
+    from_config_file,
+    from_config
+)
 
-__all__ = ['create_tsv']
+__all__ = [
+    'create_tsv',
+    'TSVBuilder',
+    'from_usfm_corpus',
+    'from_usx_corpus',
+    'from_tsv',
+    'from_config_file',
+    'from_config'
+]
